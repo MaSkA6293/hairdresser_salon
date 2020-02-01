7e51c0cb535ae79b55cbddbe29af589648b13ed7
MaSkA6293 <MaSkA6293@yandex.ru>
2020-02-01 16:00:51 +0300

    start work

---
+++ src/PageInterjer/PageInterjer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './PajeInterjer.css';
+
+
+class PageInterjer extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+
+        )
+    }
+}
+
+export default PageInterjer;
